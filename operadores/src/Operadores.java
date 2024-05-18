a51e2fd0390f4a57574abe92a6d3c90debc831df
luigithiago <luigithiago13@hotmail.com>
2024-05-18 15:44:36 -0300

    Commitando novos códigos focados em operadores

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        if(condicao1 && condicao2)
+	System.out.print("Os dois valores precisam ser verdadeiros");;
+
+
+        if(condicao1 || condicao2)
+	System.out.print("Um dos valores precisa ser verdadeiro");
+
+
+    }
+}
